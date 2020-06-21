55c5076538f5bf65af769ab14e984c6632cc7da1
kolev77 <rostislav.kolev77@gmail.com>
2020-06-22 00:42:42 +0300

    Change in file structure

--- src/main/java/home/knightforthewin/RunGame.java
+++ src/main/java/home/knightforthewin/RunGame.java
@@ -16,7 +16,7 @@ import java.util.Scanner;
  */
 public class RunGame {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         KnightForTheWin knightForTheWin = new KnightForTheWin();
         Cell startingCell = processInput();
         Knight knight = new Knight(startingCell);
